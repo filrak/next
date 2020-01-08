d044cf025523626ca0a345780daabd9d00cc6730
Filip Rakowski <frakowski@divante.pl>
2020-01-08 09:55:09 +0100

    change ref to reactive

--- packages/commercetools/theme/assets/ui-state.ts
+++ packages/commercetools/theme/assets/ui-state.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
-import VueCompositionAPI, { ref, computed } from '@vue/composition-api'
+import VueCompositionAPI, { reactive, computed } from '@vue/composition-api'
 
 // We need to register it again because of Vue instance instantiation issues
 Vue.use(VueCompositionAPI)
 
-const state = ref({ 
+const state = reactive({ 
   isCartSidebarOpen: false  
 })
 
-const isCartSidebarOpen = computed(() => state.value.isCartSidebarOpen )
-const toggleCartSidebar = () => { state.value.isCartSidebarOpen = !state.value.isCartSidebarOpen }
+const isCartSidebarOpen = computed(() => state.isCartSidebarOpen )
+const toggleCartSidebar = () => { state.isCartSidebarOpen = !state.isCartSidebarOpen }
 
 export { isCartSidebarOpen, toggleCartSidebar }
