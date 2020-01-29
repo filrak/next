a36d30e53fe10ca64a3a54f643a94d950889a642
Filip Rakowski <frakowski@divante.pl>
2020-01-29 14:59:47 +0100

    useCategory, useProduct

--- packages/boilerplate/composables/types.ts
+++ packages/boilerplate/composables/types.ts
@@ -1,2 +1,4 @@
 export type Cart = {}
 export type ProductVariant = {}
+export type Category = {}
+export type CategoryFilter = {}
--- packages/boilerplate/composables/useProduct/index.ts
+++ packages/boilerplate/composables/useProduct/index.ts
@@ -2,6 +2,9 @@ import { UseProduct } from '@vue-storefront/interfaces'
 import { ref, Ref } from '@vue/composition-api'
 import { ProductVariant } from './../types'
 
+// Product-specific typings. 
+// Those inetrfaces are just recommendations. 
+// Feel free to update them to match your platform specification.
 type Search = (params: any) => void
 
 export default function useProduct (): UseProduct<ProductVariant, Search> {
