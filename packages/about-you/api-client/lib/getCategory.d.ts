ff23917276f650fde88d3f50f1714a99081ce359
Filip Rakowski <frakowski@divante.pl>
2020-02-12 09:21:17 +0100

    remove lib

--- packages/about-you/api-client/lib/getCategory.d.ts
+++
@@ -1,7 +0,0 @@
-import { CategoryWith } from '@aboutyou/backbone/types/CategoryWith';
-export default function (options: {
-    ids: number[];
-    path: string;
-    with: CategoryWith;
-    depth: number;
-} | any): Promise<any>;
