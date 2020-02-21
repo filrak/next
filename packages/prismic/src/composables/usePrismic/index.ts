65dd5d53623256940b39d2a1f3d9b21c0661354e
Marcin Drwiega <mdrwiega@divante.pl>
2020-02-21 10:37:19 +0100

    Moved loadDocuments function above copmosable

--- packages/prismic/src/composables/usePrismic/index.ts
+++ packages/prismic/src/composables/usePrismic/index.ts
@@ -28,29 +28,29 @@ interface UsePrismic {
   search: Search;
 }
 
+const loadDocuments = async (query: PrismicQuery | PrismicQuery[], options: OptionsType = {}, getFirst: boolean): Promise<LoadDocuments> => {
+  const api = await prismic.getApi(endpoint, apiOptions);
+
+  if (getFirst) {
+    return {
+      results: await api.queryFirst(transformQuery(query), options),
+      metadata: null
+    };
+  }
+
+  const { results, ...metadata } = await api.query(transformQuery(query), options);
+
+  return {
+    results,
+    metadata
+  };
+};
+
 export default function usePrismic(): UsePrismic {
   const loading = ref(false);
   const error = ref(null);
   const doc: Ref<Document | Document[]> = ref({});
   const meta: Ref<PrismicMeta | null> = ref(null);
-
-  const loadDocuments = async (query: PrismicQuery | PrismicQuery[], options: OptionsType = {}, getFirst: boolean): Promise<LoadDocuments> => {
-    const api = await prismic.getApi(endpoint, apiOptions);
-
-    if (getFirst) {
-      return {
-        results: await api.queryFirst(transformQuery(query), options),
-        metadata: null
-      };
-    }
-
-    const { results, ...metadata } = await api.query(transformQuery(query), options);
-
-    return {
-      results,
-      metadata
-    };
-  };
 
   const search: Search = async (query: PrismicQuery | PrismicQuery[], options: OptionsType = {}, getFirst = false) => {
     loading.value = true;
