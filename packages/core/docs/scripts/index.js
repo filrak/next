79b8ee2ad7165109d77489f9db77799352f65112
Roland <rolandolah@icloud.com>
2020-03-05 10:45:15 +0100

    Refactors logging

--- packages/core/docs/scripts/index.js
+++ packages/core/docs/scripts/index.js
@@ -2,6 +2,15 @@
 const path = require('path');
 const apiExtractor = require('@microsoft/api-extractor');
 const fs = require('fs');
+const consola = require('consola');
+const chalk = require('chalk');
+
+const baseConfig = require('./baseconfig');
+const integrationMap = {
+  'about-you': 'ay',
+  commercetools: 'ct',
+  boilerplate: 'bp'
+};
 
 const pathResolver = (arg) => path.resolve(__dirname, arg);
 
@@ -10,7 +19,11 @@ const integrations = ['about-you', 'commercetools'];
 const modules = ['api-client', 'composables'];
 const rootPath = pathResolver('../../../../packages');
 
-const baseConfig = require('./baseconfig');
+const log = {
+  info: (message) => consola.info(chalk.bold('API-EXTRACTOR:'), message),
+  success: (message) => consola.success(chalk.bold('API-EXTRACTOR:'), message),
+  error: (message) => consola.error(chalk.bold('API-EXTRACTOR:'), message)
+};
 
 function createApiDoc(integration) {
   modules.forEach((module) => {
@@ -19,8 +32,7 @@ function createApiDoc(integration) {
 
     // check of module exists
     if (!fs.existsSync(moduleRootPath)) {
-      console.log(`Module: ${module} does not exist for Integration: ${integration}`);
-      process.exitCode = 0;
+      log.error(`The module ${module} does not exist for integration: ${integration}.`);
     } else {
       if (!fs.existsSync(pathResolver(`${moduleRootPath}/docs`))) {
         fs.mkdirSync(pathResolver(`${moduleRootPath}/docs`));
@@ -28,7 +40,11 @@ function createApiDoc(integration) {
       if (!fs.existsSync(pathResolver(`${moduleRootPath}/docs/temp`))) {
         fs.mkdirSync(pathResolver(`${moduleRootPath}/docs/temp`));
       }
-      if (!fs.existsSync(pathResolver(mainEntryPointFilePath))) {
+      if (!fs.existsSync(pathResolver(`${moduleRootPath}/lib/index.d.ts`))) {
+        if (!fs.existsSync(pathResolver(`${moduleRootPath}/lib/src/index.d.ts`))) {
+          log.error(`Entrypoint for ${module} not found. Try running yarn build:${integrationMap[integration]}:${module} before.`);
+          process.exit(1);
+        }
         mainEntryPointFilePath = pathResolver(`${moduleRootPath}/lib/src/index.d.ts`);
       }
 
@@ -57,13 +73,12 @@ function createApiDoc(integration) {
       });
 
       if (extractorResult.succeeded) {
-        console.log('API Extractor completed successfully');
+        log.success('API Extractor completed successfully.');
         fs.unlinkSync(path.join(__dirname, `../${configFileName}`));
-        process.exitCode = 0;
       } else {
-        console.log(`API Extractor completed with ${extractorResult.errorCount} errors` + ` and ${extractorResult.warningCount} warnings`);
+        log.error(`API Extractor completed with ${extractorResult.errorCount} errors` + ` and ${extractorResult.warningCount} warnings.`);
         fs.unlinkSync(path.join(__dirname, `../${configFileName}`));
-        process.exitCode = 1;
+        process.exit(1);
       }
     }
   });
