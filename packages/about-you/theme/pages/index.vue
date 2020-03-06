444dd0781d8cbe4586d98e6d08b94143e0272736
Roland <rolandolah@icloud.com>
2020-03-06 10:00:12 +0100

    Adds default theme

---
+++ packages/about-you/theme/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <button @click="callBapi">HELLO</button>
+    <div>{{ product }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { setup, getProduct } from '@vue-storefront/about-you-api';
+setup();
+
+export default {
+  data: () => ({
+    product: null
+  }),
+
+  methods: {
+    async callBapi() {
+      const product = await getProduct({ id: 3769861 });
+      this.product = product;
+      return 'foo';
+    }
+  }
+};
+</script>
+
+<style>
+</style>
