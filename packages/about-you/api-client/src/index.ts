ca6ce74f413504a2e6b1bd76f67ba8717d4fd524
Roland <rolandolah@icloud.com>
2020-03-09 14:01:45 +0100

    Undo changes

--- packages/about-you/api-client/src/index.ts
+++ packages/about-you/api-client/src/index.ts
@@ -1,10 +1,26 @@
 import { BapiClient } from '@aboutyou/backbone';
 import getProductApi from './getProduct';
+import getCategoryApi from './getCategory';
+import addToCartApi from './addToCart';
+import removeFromCartApi from './removeFromCart';
+import clearCartApi from './clearCart';
+import placeOrderApi from './placeOrder';
+import getUserApi from './getUser';
+import addCouponApi from './addCoupon';
+import removeCouponApi from './removeCoupon';
 
-let api: BapiClient = null;
+let api = null;
 
 let methods = {
-  getProduct: getProductApi
+  getProduct: getProductApi,
+  getCategory: getCategoryApi,
+  addToCart: addToCartApi,
+  removeFromCart: removeFromCartApi,
+  clearCart: clearCartApi,
+  placeOrder: placeOrderApi,
+  getUser: getUserApi,
+  addCoupon: addCouponApi,
+  removeCoupon: removeCouponApi
 };
 
 function override(overrides) {
@@ -14,19 +30,42 @@ function override(overrides) {
 
 function setup() {
   api = new BapiClient({
-    host: 'https://mop.azure-api.net/bapi/',
-    auth: { username: 'aboutyou', password: 'OmNErAb96Y5Qn75SFhXr' },
+    host: 'https://boston.backbone-api.demo.aboutyou.cloud/v1/',
+    auth: { username: 'aboutyou',
+      password: 'OmNErAb96Y5Qn75SFhXr' },
     shopId: 121
   });
 }
 
 /** just because you can't simply do "export x as y..." */
 const getProduct = methods.getProduct;
+const getCategory = methods.getCategory;
+const addToCart = methods.addToCart;
+const removeFromCart = methods.removeFromCart;
+const clearCart = methods.clearCart;
+const placeOrder = methods.placeOrder;
+const getUser = methods.getUser;
+const addCoupon = methods.addCoupon;
+const removeCoupon = methods.removeCoupon;
+
+setup();
+async function getCat() {
+  const cat = await getCategory({ depth: 3 });
+  console.log(JSON.stringify(cat));
+}
+getCat();
 
 export {
   getProduct,
+  getCategory,
+  addToCart,
+  removeFromCart,
+  clearCart,
+  placeOrder,
+  getUser,
+  addCoupon,
+  removeCoupon,
   override,
   setup,
   api
 };
-
