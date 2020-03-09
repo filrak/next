baef25dc111d0c78543c71297efd77bf7e0e2937
Roland <rolandolah@icloud.com>
2020-03-09 16:10:03 +0100

    Updates getCategory

--- packages/about-you/api-client/src/getCategory.ts
+++ packages/about-you/api-client/src/getCategory.ts
@@ -1,9 +1,9 @@
 
-import { api } from './index';
+import { api } from './';
 import { CategoryWith } from '@aboutyou/backbone/types/CategoryWith';
 import { BapiCategory } from '@aboutyou/backbone/types/BapiCategory';
 
-export type getCategorySearchParams = { ids: number[]; path: string; with: CategoryWith; depth: number}
+export type getCategorySearchParams = { ids: number[]; path: string; with: CategoryWith; depth: number; includeHidden: boolean }
 
 export default async function(options: getCategorySearchParams): Promise<BapiCategory[]> {
   if (!options) {
@@ -12,10 +12,13 @@ export default async function(options: getCategorySearchParams): Promise<BapiCat
   if (options.ids) {
     return await api.categories.getByIds(
       options.ids,
-      { with: options.with }
+      {
+        with: options.with,
+        includeHidden: options.includeHidden
+      }
     );
   } else if (options.path) {
-    const response = await api.ctagories.getByPath(
+    const response = await api.categories.getByPath(
       options.path,
       { width: options.with }
     );
