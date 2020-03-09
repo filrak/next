245336551356bcc79864ef294686aae834b08a7b
Roland <rolandolah@icloud.com>
2020-03-09 14:22:19 +0100

    Refactors getProduct

--- packages/about-you/api-client/src/getProduct.ts
+++ packages/about-you/api-client/src/getProduct.ts
@@ -5,47 +5,42 @@ import { BapiProduct } from '@aboutyou/backbone/types/BapiProduct';
 import { ProductSortConfig } from '@aboutyou/backbone/endpoints/products/products';
 import { Pagination } from '@aboutyou/backbone/endpoints/products/productsByIds';
 
-type getProductSearchParams = { ids: number[]; pWith: ProductWith; where: ProductSearchQuery; sort: ProductSortConfig; pagination: Pagination; masterKey: string; campaignKey; term: string };
+type getProductSearchParams = { ids: number[]; with: ProductWith; where: ProductSearchQuery; sort: ProductSortConfig; pagination: Pagination; masterKey: string; campaignKey; term: string };
 
-export default async function(params: getProductSearchParams): Promise<BapiProduct[]> {
-  let {
-    pWith,
-    where,
-    sort,
-    campaignKey
-  } = params;
-
-  if (!campaignKey) {
-    campaignKey = '';
-  }
-  if (!pWith) {
-    pWith = {};
-  }
-  if (!where) {
-    where = {};
-  }
-  if (!sort) {
-    sort = {};
+export default async function(options: getProductSearchParams): Promise<BapiProduct[]> {
+  if (!options) {
+    this.options = {};
   }
 
-  const {
-    ids,
-    masterKey,
-    pagination,
-    term
-  } = params;
-
-  if (ids) {
-    const response = await api.products.getByIds(ids, { with: pWith, campaignKey });
-    return response;
-  } else if (masterKey) {
-    const response = await api.masters.getByKey(masterKey, { with: { products: pWith }, campaignKey });
-    return response.products;
-  } else if (term) {
-    const response = await api.search.suggestions(term, campaignKey);
-    return response.products;
+  if (options.ids) {
+    return await api.products.getByIds(
+      options.ids,
+      {
+        with: options.with,
+        campaignKey: options.campaignKey
+      }
+    );
+  } else if (options.masterKey) {
+    return await api.masters.getByKey(
+      options.masterKey,
+      {
+        with: {
+          products: options.with
+        },
+        campaignKey: options.campaignKey
+      }).products;
+  } else if (options.term) {
+    return await api.search.suggestions(
+      options.term,
+      options.campaignKey
+    ).products;
   } else {
-    const response = await api.products.query({ where, with: pWith, sort, pagination });
-    return response.entities;
+    return await api.products.query(
+      {
+        where: options.where,
+        with: options.with,
+        sort: options.sort,
+        pagination: options.pagination
+      }).entities;
   }
 }
