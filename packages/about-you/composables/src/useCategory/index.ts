53119177d85fd4f106e34ce5e91f69441a3373b2
Roland Olah <rolandolah@icloud.com>
2020-03-12 19:47:40 +0100

    Udpdate ay composables

--- packages/about-you/composables/src/useCategory/index.ts
+++ packages/about-you/composables/src/useCategory/index.ts
@@ -3,16 +3,8 @@ import { getCategory } from '@vue-storefront/about-you-api';
 import { BapiCategory } from '@aboutyou/backbone/types/BapiCategory';
 import { useCategoryFactory } from '@vue-storefront/factories';
 
-const loadBapiCategories = async (params): Promise<BapiCategory[]> => {
-  const searchParams = {
-    ...params
-  };
-
-  return await getCategory(searchParams);
-};
-
 const useCategory: (id: string) => UseCategory<BapiCategory> = useCategoryFactory<BapiCategory, any>({
-  categorySearch: loadBapiCategories
+  categorySearch: getCategory
 });
 
 export default useCategory;
--- packages/about-you/composables/tests/useCategory/index.spec.ts
+++ packages/about-you/composables/tests/useCategory/index.spec.ts
@@ -1,33 +1,20 @@
 import useCategory from './../../src/useCategory';
 
-const mockedResponse = [
-  { name: 'cat1', id: 'bbb' },
-  { name: 'cat2', id: 'aaa' },
-  { name: 'cat3', id: 'fcd' }
-];
-
 jest.mock('@vue-storefront/about-you-api', () => ({
   getCategory: () =>
-    Promise.resolve({
-      data: mockedResponse
-    })
+    Promise.resolve({})
+}));
+
+jest.mock('@vue-storefront/factories', () => ({
+  useCategoryFactory: jest.fn(() => () => ({ foo: 'bar' }))
 }));
 
 describe('[about-you-composables] useCategory', () => {
-  it('creates properties', () => {
-    const { categories, loading } = useCategory('test-category');
-
-    expect(categories.value).toEqual([]);
-    expect(loading.value).toEqual(false);
+  it('is type of function', () => {
+    expect(useCategory).toBeInstanceOf(Function);
   });
 
-  it('returns category response', async () => {
-    const { search, categories, loading } = useCategory('test-category');
-
-    expect(loading.value).toBeFalsy();
-    await search({ path: ['category-slug'] });
-
-    expect(categories.value).toEqual(mockedResponse);
-    expect(loading.value).toBeFalsy();
+  it('returns value of factory execution', () => {
+    expect(useCategory('test')).toEqual({ foo: 'bar' });
   });
 });
