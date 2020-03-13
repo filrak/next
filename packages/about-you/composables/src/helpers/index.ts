00c07353f57a30d50fb921e5a8b474c323720e9c
Roland Olah <rolandolah@icloud.com>
2020-03-13 09:07:27 +0100

    Refactors getProduct

---
+++ packages/about-you/composables/src/helpers/index.ts
@@ -0,0 +1,4 @@
+import enhanceProduct from './enhanceProduct';
+export {
+  enhanceProduct
+};
--- packages/about-you/composables/src/useProduct/index.ts
+++ packages/about-you/composables/src/useProduct/index.ts
@@ -1,21 +1,7 @@
 import { UseProduct } from '@vue-storefront/interfaces';
-import { getProduct } from '@vue-storefront/about-you-api';
 import { BapiProduct } from '@aboutyou/backbone/types/BapiProduct';
 import { useProductFactory } from '@vue-storefront/factories';
-
-const loadBapiProducts = async (params): Promise<BapiProduct[]> => {
-  const searchParams = {
-    ids: params.ids,
-    with: params.term,
-    where: params.term,
-    sort: params.sort,
-    page: params.pagination.page,
-    masterKey: '',
-    term: params.term
-  };
-
-  return await getProduct(searchParams);
-};
+import loadBapiProducts from '../helpers';
 
 const useProduct: (cacheId: string) => UseProduct<BapiProduct> = useProductFactory<BapiProduct, any>({
   productsSearch: loadBapiProducts
