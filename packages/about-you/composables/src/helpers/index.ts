339787ea38ec2829d69ac33f6deeafa17be5faeb
Roland Olah <rolandolah@icloud.com>
2020-03-13 09:27:53 +0100

    Fixed typo

--- packages/about-you/composables/src/helpers/index.ts
+++ packages/about-you/composables/src/helpers/index.ts
@@ -1,4 +1,5 @@
 import enhanceProduct from './enhanceProduct';
+
 export {
   enhanceProduct
 };
--- packages/about-you/composables/src/useProduct/index.ts
+++ packages/about-you/composables/src/useProduct/index.ts
@@ -1,10 +1,10 @@
 import { UseProduct } from '@vue-storefront/interfaces';
 import { BapiProduct } from '@aboutyou/backbone/types/BapiProduct';
 import { useProductFactory } from '@vue-storefront/factories';
-import loadBapiProducts from '../helpers';
+import { enhanceProduct } from '../helpers';
 
 const useProduct: (cacheId: string) => UseProduct<BapiProduct> = useProductFactory<BapiProduct, any>({
-  productsSearch: loadBapiProducts
+  productsSearch: enhanceProduct
 });
 
 export default useProduct;
