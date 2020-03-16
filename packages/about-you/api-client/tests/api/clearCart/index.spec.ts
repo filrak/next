c1b756e8519ae1b9e707e84a1b80d9f9995f29ee
Roland Olah <rolandolah@icloud.com>
2020-03-16 11:47:37 +0100

    Updates tests

--- packages/about-you/api-client/tests/api/clearCart/index.spec.ts
+++ packages/about-you/api-client/tests/api/clearCart/index.spec.ts
@@ -1,46 +1,27 @@
 import clearCart from '../../../src/api/clearCart';
 import { apiClient } from '../../../src/index';
 
-jest.mock('../../../src/index');
-
 describe('[about-you-api-client] clearCart', () => {
-  it('clears cart', async () => {
+  it('adds item to cart', async () => {
     const mockGetBasket = jest.fn(() => Promise.resolve({
-      type: 'success',
       basket: {
-        key: '',
-        cost: 0,
-        currencyCode: 'EUR',
         items: [
           {
             key: 'test'
-          }
-        ],
-        packages: [
-          {
-            id: 1,
-            carrierKey: '',
-            deliveryDate: {
-              max: '',
-              min: ''
-            }
           }
         ]
       }
     }));
 
-    const mockDeleteItem = jest.fn((params) => params);
+    const mockClearCart = jest.fn((params) => params);
 
-    (apiClient as any as jest.Mock).mockImplementation(() => ({
-      basket: {
-        get: mockGetBasket,
-        deleteItem: mockDeleteItem
-      }
-    }));
+    // @ts-ignore
+    apiClient.basket.get = mockGetBasket;
+    apiClient.basket.deleteItem = mockClearCart;
 
     await clearCart('basket-key');
 
     expect(mockGetBasket).toHaveBeenCalledWith('basket-key');
-    expect(mockDeleteItem).toHaveBeenCalledWith('basket-key', 'test');
+    expect(mockClearCart).toHaveBeenCalledWith('basket-key', 'test');
   });
 });
