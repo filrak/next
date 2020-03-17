6a4ac12339a974147f6d550cb9871f813657476f
Konrad <admin@zoovichat.com>
2020-03-17 16:40:46 +0100

    All test for useUser are done

--- packages/commercetools/composables/src/useUser/index.ts
+++ packages/commercetools/composables/src/useUser/index.ts
@@ -1,9 +1,10 @@
 import { UseUser } from '@vue-storefront/interfaces';
-import { params } from './factoryParams';
 import {
   Customer,
   Cart
 } from '@vue-storefront/commercetools-api/lib/src/types/GraphQL';
+
+import { params } from './factoryParams';
 import { useUserFactory } from '@vue-storefront/factories';
 
 const useUser: () => UseUser<Customer, any> = useUserFactory<Customer, Cart, any>(params);
---
+++ packages/commercetools/composables/tests/useUser/index.spec.ts
@@ -0,0 +1,18 @@
+jest.mock('@vue-storefront/factories', () => ({
+  useUserFactory: jest.fn(() => () => ({user: 'api'}))
+}));
+
+jest.mock('../../src/useUser/factoryParams', () => ({
+  params: {}
+}));
+
+import { useUserFactory } from '@vue-storefront/factories';
+import { params } from '../../src/useUser/factoryParams';
+import useUser from '../../src/useUser/';
+
+describe('useUser composable', () => {
+  it('returns useUserFactory functions', () => {
+    expect(useUserFactory).toHaveBeenCalledWith(params);
+    expect(useUser()).toEqual({user: 'api'});
+  });
+});
