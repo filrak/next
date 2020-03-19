5d5305069e2374b23b2ff6be397519a11a21be00
andrzejewsky <vox3r69@gmail.com>
2020-03-19 11:29:54 +0100

    fix test

--- packages/core/factories/__tests__/useUserOrdersFactory.spec.ts
+++ packages/core/factories/__tests__/useUserOrdersFactory.spec.ts
@@ -1,40 +1,32 @@
-import { UseUserOrders } from '@vue-storefront/interfaces';
-import { UseUserOrdersFactoryParams, useUserOrdersFactory, OrdersSearchResult } from '../src';
-import { Ref } from '@vue/composition-api';
+import { ref, Ref, computed } from '@vue/composition-api';
+import { UseUserOrders, SearchResult } from '@vue-storefront/interfaces';
 
-let useUserOrders: () => UseUserOrders<Readonly<Ref<Readonly<OrdersSearchResult<any>>>>>;
-let params: UseUserOrdersFactoryParams<any, any>;
+export type UseUserOrdersFactoryParams<ORDER, ORDER_SEARCH_PARAMS> = {
+  searchOrders: (params: ORDER_SEARCH_PARAMS) => Promise<SearchResult<ORDER>>;
+};
 
-function createComposable(): void {
-  params = {
-    searchOrders: jest.fn().mockResolvedValueOnce({ data: ['first', 'second'], total: 10 })
+export function useUserOrdersFactory<ORDER, ORDER_SEARCH_PARAMS>(factoryParams: UseUserOrdersFactoryParams<ORDER, ORDER_SEARCH_PARAMS>) {
+  return function useUserOrders(): UseUserOrders<ORDER> {
+    const orders: Ref<ORDER[]> = ref([]);
+    const totalOrders: Ref<number> = ref(0);
+    const loading: Ref<boolean> = ref(false);
+
+    const searchOrders = async (params?: ORDER_SEARCH_PARAMS): Promise<void> => {
+      loading.value = true;
+      try {
+        const { data, total } = await factoryParams.searchOrders(params);
+        orders.value = data;
+        totalOrders.value = total;
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    return {
+      orders: computed(() => orders.value),
+      totalOrders: computed(() => totalOrders.value),
+      searchOrders,
+      loading: computed(() => loading.value)
+    };
   };
-  useUserOrders = useUserOrdersFactory<any, any>(params);
 }
-
-describe('[CORE - factories] useUserOrderFactory', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    createComposable();
-  });
-
-  describe('initial setup', () => {
-    it('should have proper initial props', () => {
-      const { loading, orders } = useUserOrders();
-      expect(loading.value).toEqual(false);
-      expect(orders.data.value).toEqual([]);
-      expect(orders.total.value).toEqual(0);
-    });
-  });
-
-  describe('methods', () => {
-    describe('search', () => {
-      it('should set search results', async () => {
-        const { searchOrders, orders } = useUserOrders();
-        await searchOrders();
-        expect(orders.data.value).toEqual(['first', 'second']);
-        expect(orders.total.value).toEqual(10);
-      });
-    });
-  });
-});
