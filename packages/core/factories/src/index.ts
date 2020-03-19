e6eeae76d4e68852a123587a379f81a8be1dfc1e
konradovsky <konrad.trade@gmail.com>
2020-03-19 09:55:54 +0100

    All CR task done - test 100%

--- packages/core/factories/src/index.ts
+++ packages/core/factories/src/index.ts
@@ -1,8 +1,5 @@
-import Vue from 'vue';
-import CompositionApi from '@vue/composition-api';
-Vue.use(CompositionApi);
-
 export * from './useCartFactory';
 export * from './useCategoryFactory';
 export * from './useProductFactory';
+export * from './useUserFactory';
 export * from './useUserOrdersFactory';
