680dfccaee5ace939bb2ceea7736d59ed21b5299
andrzejewsky <vox3r69@gmail.com>
2020-03-19 11:34:39 +0100

    fix test

--- packages/core/factories/__tests__/useUserOrdersFactory.spec.ts
+++ packages/core/factories/__tests__/useUserOrdersFactory.spec.ts
@@ -1,32 +1,49 @@
-import { ref, Ref, computed } from '@vue/composition-api';
 import { UseUserOrders, SearchResult } from '@vue-storefront/interfaces';
+import { UseUserOrdersFactoryParams, useUserOrdersFactory } from '../src';
+import { Ref } from '@vue/composition-api';
 
-export type UseUserOrdersFactoryParams<ORDER, ORDER_SEARCH_PARAMS> = {
-  searchOrders: (params: ORDER_SEARCH_PARAMS) => Promise<SearchResult<ORDER>>;
-};
+let useUserOrders: () => UseUserOrders<Readonly<Ref<Readonly<SearchResult<any>>>>>;
+let params: UseUserOrdersFactoryParams<any, any>;
 
-export function useUserOrdersFactory<ORDER, ORDER_SEARCH_PARAMS>(factoryParams: UseUserOrdersFactoryParams<ORDER, ORDER_SEARCH_PARAMS>) {
-  return function useUserOrders(): UseUserOrders<ORDER> {
-    const orders: Ref<ORDER[]> = ref([]);
-    const totalOrders: Ref<number> = ref(0);
-    const loading: Ref<boolean> = ref(false);
-
-    const searchOrders = async (params?: ORDER_SEARCH_PARAMS): Promise<void> => {
-      loading.value = true;
-      try {
-        const { data, total } = await factoryParams.searchOrders(params);
-        orders.value = data;
-        totalOrders.value = total;
-      } finally {
-        loading.value = false;
-      }
-    };
-
-    return {
-      orders: computed(() => orders.value),
-      totalOrders: computed(() => totalOrders.value),
-      searchOrders,
-      loading: computed(() => loading.value)
-    };
+function createComposable(): void {
+  params = {
+    searchOrders: jest.fn().mockResolvedValueOnce({ data: ['first', 'second'], total: 10 })
   };
+  useUserOrders = useUserOrdersFactory<any, any>(params);
 }
+
+describe('[CORE - factories] useUserOrderFactory', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    createComposable();
+  });
+
+  describe('initial setup', () => {
+    it('should have proper initial props', () => {
+      const { loading, orders, totalOrders } = useUserOrders();
+      expect(loading.value).toEqual(false);
+      expect(orders.value).toEqual([]);
+      expect(totalOrders.value).toEqual(0);
+    });
+  });
+
+  describe('methods', () => {
+    describe('search', () => {
+      it('should set search results', async () => {
+        const { searchOrders, orders, totalOrders } = useUserOrders();
+        await searchOrders();
+        expect(orders.value).toEqual(['first', 'second']);
+        expect(totalOrders.value).toEqual(10);
+      });
+
+      it('should disable loading flag on error', async () => {
+        params.searchOrders = jest.fn().mockImplementationOnce(() => {
+          throw new Error();
+        });
+        const { searchOrders, loading } = useUserOrders();
+        await expect(searchOrders()).rejects.toThrow(Error);
+        expect(loading.value).toEqual(false);
+      });
+    });
+  });
+});
