b323c46ec65682b468c50ec7c9f72ff2a67ec86d
Qrzy <qrzy88@gmail.com>
2020-03-23 13:30:31 +0100

    test(commercetools): test new helper

--- packages/commercetools/composables/src/helpers/internals/mapPaginationParams.ts
+++ packages/commercetools/composables/src/helpers/internals/mapPaginationParams.ts
@@ -2,8 +2,13 @@ import { BaseSearch } from '@vue-storefront/commercetools-api/lib/src/types/Api'
 import { AgnosticPaginatedSearchParams } from '@vue-storefront/interfaces';
 
 export default (params: AgnosticPaginatedSearchParams): Pick<BaseSearch, 'limit' | 'offset'> => {
-  return {
-    limit: params.perPage,
-    offset: (params.page - 1) * params.perPage
-  };
+  let mappedParams;
+  if (params.perPage && params.page) {
+    mappedParams = {
+      limit: params.perPage,
+      offset: (params.page - 1) * params.perPage
+    };
+  }
+
+  return mappedParams;
 };
