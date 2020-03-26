58e57b3086f5456b380928bb7d03ebb53059b310
Luke Romanowicz <lromanowicz1@gmail.com>
2020-03-26 17:54:11 +0100

    refactor: locale fix tests

--- packages/commercetools/composables/__tests__/useLocale/useLocale.spec.ts
+++ packages/commercetools/composables/__tests__/useLocale/useLocale.spec.ts
@@ -10,7 +10,7 @@ import { useLocaleFactory } from '@vue-storefront/factories';
 import { params } from '../../src/useLocale/factoryParams';
 import useLocale from '../../src/useLocale';
 describe('[commercetools-composables] useLocale', () => {
-  it('returns useUserFactory functions', () => {
+  it('returns useLocale api', () => {
     expect(useLocaleFactory).toHaveBeenCalledWith(params);
     expect(useLocale()).toEqual({locale: 'en'});
   });
