fb95fb09cffd84b91996d9607ad0385ef52e28b0
Marcin Drwiega <mdrwiega@divante.pl>
2020-04-16 12:52:30 +0200

    order getters implementation

--- packages/commercetools/composables/src/getters/orderGetters.ts
+++ packages/commercetools/composables/src/getters/orderGetters.ts
@@ -1,5 +1,5 @@
 import { UserOrderGetters, AgnosticOrderStatus } from '@vue-storefront/core';
-import { Order, OrderState } from './../types/GraphQL';
+import { Order, OrderState, LineItem } from './../types/GraphQL';
 
 export const getOrderDate = (order: Order): string => order?.createdAt || '';
 
@@ -14,13 +14,25 @@ const orderStatusMap = {
 
 export const getOrderStatus = (order: Order): AgnosticOrderStatus | '' => order?.orderState ? orderStatusMap[order.orderState] : '';
 
-export const getOrderPrice = (order: Order): number | null => order ? order.totalPrice.centAmount / 100 : null;
+export const getOrderPrice = (order: Order): number => order ? order.totalPrice.centAmount / 100 : 0;
 
-const orderGetters: UserOrderGetters<Order> = {
+export const getOrderItems = (order: Order): LineItem[] => order?.lineItems || [];
+
+export const getOrderItemSku = (item: LineItem): string => item?.productId || '';
+
+export const getOrderItemName = (item: LineItem): string => item?.name || '';
+
+export const getOrderItemQty = (item: LineItem): number => item?.quantity || 0;
+
+const orderGetters: UserOrderGetters<Order, LineItem> = {
   getDate: getOrderDate,
   getId: getOrderId,
   getStatus: getOrderStatus,
-  getPrice: getOrderPrice
+  getPrice: getOrderPrice,
+  getItems: getOrderItems,
+  getItemSku: getOrderItemSku,
+  getItemName: getOrderItemName,
+  getItemQty: getOrderItemQty
 };
 
 export default orderGetters;
