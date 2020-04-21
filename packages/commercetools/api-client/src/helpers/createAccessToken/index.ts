b13fccf845183181ea38da4d053f63d33cccdb47
andrzejewsky <vox3r69@gmail.com>
2020-04-21 11:35:09 +0200

    token provider

--- packages/commercetools/api-client/src/helpers/createAccessToken/index.ts
+++ packages/commercetools/api-client/src/helpers/createAccessToken/index.ts
@@ -1,7 +1,8 @@
-import SdkAuth from '@commercetools/sdk-auth';
+import SdkAuth, { TokenProvider } from '@commercetools/sdk-auth';
 import { Token, ApiConfig, CustomerCredentials } from '../../types/setup';
-import { api } from './../../index';
+import { api, currentToken } from './../../index';
 
+// %7B%22access_token%22%3A%22uDAmp-v7A32jkaUezGwvKHlAsDyxAE_L%22%2C%22expires_in%22%3A172800%2C%22scope%22%3A%22create_anonymous_token%3Avsf-ct-dev%20manage_my_orders%3Avsf-ct-dev%20manage_my_profile%3Avsf-ct-dev%20anonymous_id%3A89038268-78ec-41a5-aeca-797cc4249c95%20manage_my_shopping_lists%3Avsf-ct-dev%20manage_my_payments%3Avsf-ct-dev%20view_products%3Avsf-ct-dev%20view_published_products%3Avsf-ct-dev%22%2C%22refresh_token%22%3A%22ZtGL5kEc_I-AKxGYpw0uYwii8sYZNtutSeuTWuiekSY%3D%22%2C%22token_type%22%3A%22Bearer%22%2C%22expires_at%22%3A1587033011585%7D
 interface FlowOptions {
   currentToken?: Token;
   customerCredentials?: CustomerCredentials;
@@ -19,44 +20,36 @@ const createAuthClient = (config: ApiConfig): SdkAuth =>
     scopes: config.scopes
   });
 
-const anonymousFlow = async (config: ApiConfig): Promise<Token> => {
-  const authClient = createAuthClient(config);
+const isValid = (token: Token) => token && Boolean(token.refresh_token);
 
-  return authClient.anonymousFlow();
-};
-
-const customerPasswordFlow = async (config: ApiConfig, credentials: CustomerCredentials): Promise<Token> => {
-  const authClient = createAuthClient(config);
-
-  return authClient.customerPasswordFlow(credentials);
-};
-
-const refreshTokenFlow = async (config: ApiConfig, refreshToken: string): Promise<Token> => {
-  const authClient = createAuthClient(config);
-
-  return authClient.refreshTokenFlow(refreshToken);
-};
-
-const isTokenExpired = (token: Token): boolean => Date.now() > token.expires_at;
-
-const createAccessToken = async (options: FlowOptions = {}): Promise<Token> => {
-  const { currentToken } = options;
-
-  if (options.customerCredentials) {
-    return customerPasswordFlow(api, options.customerCredentials);
+const getCurrentToken = (options: FlowOptions = {}) => {
+  if (currentToken) {
+    return currentToken;
   }
 
-  if (currentToken && !isTokenExpired(currentToken)) {
+  return options.currentToken;
+};
+
+const getTokenFlow = async (sdkAuth: SdkAuth, options: FlowOptions = {}) => {
+  const currentToken = getCurrentToken(options);
+
+  if (options.customerCredentials) {
+    return sdkAuth.customerPasswordFlow(options.customerCredentials);
+  }
+
+  if (isValid(currentToken)) {
     return Promise.resolve(currentToken);
   }
 
-  if (!currentToken) {
-    return anonymousFlow(api);
-  }
+  return sdkAuth.anonymousFlow();
+};
 
-  if (isTokenExpired(currentToken)) {
-    return refreshTokenFlow(api, currentToken.refresh_token);
-  }
+const createAccessToken = async (options: FlowOptions = {}): Promise<Token> => {
+  const sdkAuth = createAuthClient(api);
+  const tokenInfo = await getTokenFlow(sdkAuth, options);
+  const tokenProvider = new TokenProvider({ sdkAuth }, tokenInfo);
+
+  return tokenProvider.getTokenInfo();
 };
 
 export default createAccessToken;
